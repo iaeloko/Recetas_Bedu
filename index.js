144f3c4da70a87f19fbd0830c5b3ed37dd6d0417
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-08-30 20:41:23 -0500

    add function to get the meal information in a big card

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const btnSearchRecipe = document.getElementById('btnSearchRecipe'),
       dishes = document.querySelector('.dishes'),
       dishesContent = document.querySelector('.dishes-content'),
       dishSelectedContent = document.querySelector('.dish-selected-content'),
+      dishSelected = document.querySelector('.dish-selected'),
       btnCloseselectedDisd = document.querySelector('#btnCloseDishSelected'),
       body = document.querySelector('body');
     //   submit = document.getElementById('submit'),
@@ -24,7 +25,7 @@ function init(){
 function evenListeners(){
     // Buscar y mostrar comidas desde la API
     btnSearchRecipe.addEventListener('click', searchMeal);
-    btnCloseselectedDisd.addEventListener('click', () => showDish(null, false))
+    btnCloseselectedDisd.addEventListener('click', () => showDish(false))
 }
 
 // //Event Listener obtener comida random
@@ -51,19 +52,18 @@ function evenListeners(){
 //Función Buscar comida desde la api
 function searchMeal(event) {
     event.preventDefault();
+    cleanMealsHTML();
 
     const recipeName = txtRecipe.value; //Obtener término de búsqueda
 
     if(!recipeName.length) { showAlert('No has introducido ningun nombre de receta a buscar.', 'error'); return; }
 
-    //comprobar si está vacío
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeName}`)
         .then(response => response.json())
-        .then(data => showMeal(data));
+        .then(data => getMealData(data));
 }
 
-function showMeal(data){
-    cleanHTML();
+function getMealData(data){
     const {meals} = data;
     
     showSpinner(true);
@@ -103,7 +103,6 @@ function hoverFunctionInCards(){
     dishses.forEach( dishCard=> {
         dishCard.addEventListener('mouseenter', () => hoverSelected(dishCard, true));
         dishCard.addEventListener('mouseleave', () => hoverSelected(dishCard, false));
-        dishCard.addEventListener('click', () => showDish(dishCard, true));
     })
 }
 
@@ -111,7 +110,7 @@ function hoverFunctionInCards(){
 let scrrollOntop;
 let doc = document.documentElement;
 
-function showDish(dishCard, value){
+function showDish(value){
     if(value){
         scrrollOntop = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
         dishSelectedContent.classList.remove('no-display');
@@ -121,6 +120,7 @@ function showDish(dishCard, value){
         dishSelectedContent.classList.add('no-display');
         body.classList.remove('no-show-scroll');
         dishSelectedContent.style.top = `0px`;
+        cleanMealHTML();
     }
     
     doc.scrollTop = scrrollOntop;
@@ -184,7 +184,14 @@ function buildMealCards(mealData){
     dish.appendChild(dishImg);
     dishes.appendChild(dish);
 
+    dish.addEventListener('click', () => searchMealByID(dish));
     hoverFunctionInCards();
+}
+
+function searchMealByID(dishCard){
+    showDish(true);
+    const dishId = dishCard.getAttribute('id');
+    getMealByID(dishId);
 }
 
 function showSpinner(value){
@@ -214,7 +221,7 @@ function showAlert(message, type){
 }
 
 
-function cleanHTML(){
+function cleanMealsHTML(){
     document.querySelectorAll('.searchResultH2').forEach( searchResultH2 => {
         searchResultH2.remove();
     });
@@ -224,18 +231,119 @@ function cleanHTML(){
     }
 }
 
+function cleanMealHTML(){
+    const dataMealHtml = document.querySelector('.dish-selected-data');
+    while(dataMealHtml.children.firstChild){
+        dataMealHtml.removeChild(dataMealHtml.firstChild);
+    }
+    dataMealHtml.remove();
+}
 
-// //Función Obtener comida por ID desde la api
-// function getMealByID(mealID) {
-//     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             const meal = data.meals[0];
 
-//             addMealToDOM(meal);
-//         });
+//Función Obtener comida por ID desde la api
+function getMealByID(mealID) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+        .then(response => response.json())
+        .then(data => {
+            const meal = data.meals[0];
+            buildMealBigCard(meal);
+        });
+}
 
-// }
+function buildMealBigCard(meal){
+    console.log(meal);
+    const dish = document.createElement('div');
+    dish.classList.add('dish-selected-data');
+
+    const dishName = document.createElement('h2');
+    dishName.textContent = meal.strMeal;
+
+    const dishImg = document.createElement('img');
+    dishImg.classList.add('dish-selected-img');
+    dishImg.src = meal.strMealThumb;
+    dishImg.alt = `Imagen de la comida ${meal.strMeal}`;
+
+    const dishAreaCategory = document.createElement('div');
+    dishAreaCategory.classList.add('dish-selected-area-category');
+    dishAreaCategory.innerHTML = `<div class="dish-selected-area-category">
+                                    <div class="dish-selected-flex">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon-kitchen" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#597e8d" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <path d="M19 3v12h-5c-.023 -3.681 .184 -7.406 5 -12zm0 12v6h-1v-3m-10 -14v17m-3 -17v3a3 3 0 1 0 6 0v-3" />
+                                        </svg>
+                                        <p class="dish-selected-data__category">${meal.strCategory}</p>
+                                    </div>
+                                    <div class="dish-selected-flex">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon-place" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#597e8d" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <circle cx="12" cy="11" r="3" />
+                                            <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                                            </svg>
+                                        <p class="dish-selected-data__area">${meal.strArea}</p>
+                                    </div>
+                                </div>`;
+
+    const dishInstructions = document.createElement('div');
+    dishInstructions.classList.add('dish-selected-instructions');
+    dishInstructions.innerHTML = `<div class="dish-selected-instructions">
+                                    <h3>Instrucciones</h3>
+                                    <p>${meal.strInstructions}</p>
+                                </div>`;
+
+    const dishIngredients = document.createElement('div');
+    const dishIngredientsTitle = document.createElement('h3');
+    const dishIngredientsUl = document.createElement('ul');
+    dishIngredientsUl.classList.add('indredients-list');
+    dishIngredients.classList.add('dish-selected-ingredients');
+    dishIngredientsTitle.textContent = 'Ingredientes';
+
+    dishIngredients.appendChild(dishIngredientsTitle);
+
+    const ingredientsAndMeasure = getIngredientsAndMasures(meal);
+
+    ingredientsAndMeasure.forEach( (ingredientAndMeasure) => {
+        const dishIngredient = document.createElement('li');
+        dishIngredient.classList.add('indredients-list-item');
+        dishIngredient.textContent = `${ingredientAndMeasure[0]} - ${ingredientAndMeasure[1]}`;
+        dishIngredientsUl.appendChild(dishIngredient);
+    });
+    
+    dishIngredients.appendChild(dishIngredientsUl);
+
+    dish.appendChild(dishName);
+    dish.appendChild(dishImg);
+    dish.appendChild(dishAreaCategory);
+    dish.appendChild(dishInstructions);
+    dish.appendChild(dishIngredients);
+
+    dishSelected.appendChild(dish);
+
+}
+
+function getIngredientsAndMasures(meal){
+    const ingredientPrefix = 'strIngredient';
+    const measurePrefix = 'strMeasure';
+
+    const ingredientsAndMeasure = [];
+
+    let i = 1;
+    let ingredient = `${meal[`${ingredientPrefix}${i}`]}`;
+    let measure = `${meal[`${measurePrefix}${i}`]}`;
+    ingredientsAndMeasure.push([ingredient, measure]);
+
+    getIngredients:
+    while(ingredient.length > 0){
+        i++;
+        ingredient = `${meal[`${ingredientPrefix}${i}`]}`;
+        measure = `${meal[`${measurePrefix}${i}`]}`;
+
+        if(ingredient.length === 0 || ingredient === 'undefined' || ingredient === 'null') break getIngredients;
+
+        ingredientsAndMeasure.push([ingredient, measure]);
+    }
+
+    return ingredientsAndMeasure;
+}
 
 // //Function comida aleatoria
 // function getRandomMeal() {
