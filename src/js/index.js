4c420303c2c8430900fbca000dc134f520ffe71e
Yael Villarreal <iaeloko@gmail.com>
2023-02-23 00:02:07 -0600

    tamaños

--- src/js/index.js
+++ src/js/index.js
@@ -15,7 +15,7 @@ const btnSearchRecipe = document.getElementById('btnSearchRecipe'),
     dishSelected = document.querySelector('.dish-selected'),
     btnCloseselectedDisd = document.querySelector('#btnCloseDishSelected'),
     body = document.querySelector('body');
-    txtRecipe.placeholder = "Write an ingredient for your recipe";
+    txtRecipe.placeholder = "Write an ingredient";
 
 
 
